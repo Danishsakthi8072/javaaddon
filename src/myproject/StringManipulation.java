8e12ee252f0e3699fada717f666fc026efe42504
Danishsakthi8072 <danishsakthi8072@gmail.com>
2025-03-21 12:26:49 +0530

    Add files via upload

---
+++ src/myproject/StringManipulation.java
@@ -0,0 +1,41 @@
+package myproject;
+import java.util.Arrays;
+
+
+public class StringManipulation {
+	public static void main(String args[])
+	{
+		{
+		//litral way to asign valur
+		String str = "Danish";
+		//Asign valu eusing object
+		String obj = new String("Danish");
+		//check the memory space 
+		System.out.println(str==obj);
+		//check the value of variab;e
+		System.out.println(str.equals(obj));
+		System.out.println(str.toLowerCase());
+		System.out.println(str.toUpperCase());
+		char a = str.charAt(0);
+		System.out.println(str.charAt(0));
+		System.out.println(a);
+		System.out.println(str.indexOf(97,0));
+		System.out.println(str.lastIndexOf('a'));
+		System.out.println(str.substring(0,3));
+		char[] b = str.toCharArray();
+		System.out.println(b);
+		System.out.println(str.toCharArray());
+//		System.out.println(str.split("a"));
+		String[] d = str.split("");
+		System.out.println(Arrays.toString(d));
+		System.out.println(str.trim());
+		System.out.println(str.isBlank());
+		System.out.println(str.isEmpty());
+		System.out.println(str.concat("D"));
+		System.out.println(str.replace('a',' '));
+		System.out.println(str.replaceAll("(.*)an(.*)"," "));
+		System.out.printf("%c,%s", a,String.valueOf(b));
+		}
+			
+	}
+}
